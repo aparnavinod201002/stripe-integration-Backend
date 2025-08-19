2493044a40e5e363d1d409a0cbbf7641ff60a484
Aparna Vinod <aparnavinod083@gmail.com>
2025-08-19 20:19:42 +0530

    updated

--- Routes/paymentRoutes.js
+++ Routes/paymentRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require("body-parser");
 const controller = require('../Controller/paymentController');
+const auth = require('../Middleware/authMiddleware')
 
 router.post('/user', controller.user);
 router.post('/login', controller.login);
-router.post('/paymentNew', controller.payment);
-router.post('/payment', controller.createCheckoutSession);
+router.post('/paymentNew',controller.payment);
+router.post('/payment',auth,controller.createCheckoutSession);
 router.post(
   "/webhook",
   bodyParser.raw({ type: "application/json" }),

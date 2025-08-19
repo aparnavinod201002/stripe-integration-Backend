c25d4858e8f85a961321c138b8e344065d014cec
Aparna Vinod <aparnavinod083@gmail.com>
2025-08-19 19:13:08 +0530

    updated

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
+const allowedOrigins = [   // local frontend
+  "https://stripe-integration-frontend-rouge.vercel.app"   // deployed frontend
+];
 const routes = require('./Routes/paymentRoutes');
 require("./Config/db");
 app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
 app.use(express.json());
